6d6b174da216f3367c5ac273fdbe1f5f8c1718b6
Pasita24 <benjapaz024@gmail.com>
2023-08-23 20:33:25 -0400

    Actualizacion
    Terminado el ejercicio numero 5

--- Ej5.c
+++ Ej5.c
@@ -1,21 +1,50 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+int comparar_ascendente(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int comparar_descendente(const void *a, const void *b) {
+    return (*(int *)b - *(int *)a);
+}
+
 int main() {
     srand(time(NULL));
-    //Arreglo de numeros randoms
-    int Aleat[5];
-    //Generamos los 5 aleatorios
-    for(int i =0;i<5;i++)
-    {
-        Aleat[i] = rand()%50+1;
-    }
-    //Mostramos los numeros
-    printf("Numeros generados:\n");
-    for(int i=0;i<5;i++)
-    {
 
+    int Aleat[5];
+    int i;
+    //Generamos 5 randoms 
+    printf("---------Bienvenido----------\n");
+    for (i = 0; i < 5; i++) {
+        Aleat[i] = rand() % 100; 
     }
-    return 0; 
+    printf("Los numeros generados son:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", Aleat[i]);
+    }
+    printf("\n");
+    printf("\n");
+
+    //Metodo Ascendente
+    qsort(Aleat, 5, sizeof(int), comparar_ascendente);
+
+    printf("Numeros ascendente:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", Aleat[i]);
+    }
+    printf("\n");
+    printf("\n");
+    //Metodo descendente 
+    qsort(Aleat, 5, sizeof(int), comparar_descendente);
+
+    printf("Numeros descendente:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", Aleat[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
+
